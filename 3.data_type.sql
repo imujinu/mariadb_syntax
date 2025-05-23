51434666a40a650c2d4afdfafdc56456261d18a8
imujinu <wlsdnrhdwkd@gmail.com>
2025-05-23 17:23:21 +0900

    0523end

--- 3.data_type.sql
+++ 3.data_type.sql
@@ -40,3 +40,38 @@ alter table post add column created_time datetime;
 insert into post(id, title, author_id ) values(7, 'hello', 3);
 alter table post add column created_time datetime default current_timestamp();
 
+-- 비교 연산자
+select * from author where id >=2 and id<=4;
+select * from author where id >=2 and id<=4;
+select * from author where id in (2,3,4);
+
+-- like : 특정 문자를 포함하는 데이터를 조회하기 위한 키워드
+select * from post where title like 'h%';
+select * from post where title not like 'h%';
+select * from post where title like '%h';
+select * from post where title like '%h%';
+
+-- regexp : 정규포현식을 활용한 조회
+select * from post where title regexp '[a-z]' -- 하나라도 알파벳 소문자가 들어이으면
+select * from post where title regexp '[가-힣]' -- 하나라도 한글이 있으면
+
+-- 숫자 -> 날짜
+select cast(20250523 as date) from author; -- 2025-05-23
+-- 문자 -> 날짜
+select cast('20250523' as date) from author; -- 2025-05-23
+-- 문자 -> 숫자
+select cast('12' as unsigned);
+
+-- 날짜조회 방법 : 2025-05-23 14:30:25
+--like패턴, 부동호 활용, date_format 
+select * from post where created_time like '2025-05%'; -- 문자열처럼 조회 
+-- 5월 1일부터, 5월 20일까지. 날짜만 입력 시 시간부분은 00:00:00이 자동으로 붙음 
+select * from post where '2025-05-01'<= created_time and created_time < '2025-05-21';
+select * from post where created_time between '2025-05-01' and  '2025-05-20';
+
+select date_format(created_time, '%Y-%m-%d') from post;
+select date_format(created_time, '%H-%i-%s') from post;
+select * from post where date_format(created_time, '%m') = '05';
+
+select * from post where cast(date_format(created_time, '%m') as unsigned) = '05';
+
