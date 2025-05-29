612829d2b12f8edad3ad0ff97a4223ec72b50338
imujinu <wlsdnrhdwkd@gmail.com>
2025-05-29 15:17:12 +0900

    board sql

---
+++ 10.board.renewal.sql
@@ -0,0 +1,48 @@
+-- 회원가입 및 주소생성
+DELIMITER //
+create procedure insert_author(in emailInput varchar(255), in nameInput varchar(255), in passwordInput varchar(255),in countryInput varchar(255), in cityInput varchar(255), in streetInput varchar(255))
+begin
+    declare exit handler for SQLEXCEPTION
+    begin
+        rollback;
+    end;
+    start transaction;
+    insert into author(email, name, password) values (emailInput, nameInput, passwordInput);
+    insert into address(author_id, country, city, street) values((select id from author order by id desc limit 1) , countryInput, cityInput, streetInput);
+    commit;
+end //
+DELIMITER ;
+
+-- 글쓰기
+DELIMITER //
+create procedure insert_post(in titleInput varchar(255), in contentsInput varchar(255), in emailInput varchar(255))
+begin
+    declare exit handler for SQLEXCEPTION
+    begin
+        rollback;
+    end;
+    start transaction;
+    insert into post(title, contents) values (titleInput, contentsInput);
+    insert author_post(author_id, post_id) values((select id from author where email=emailInput), (select id from post order by id desc limit 1));
+    commit;
+end //
+DELIMITER ;
+
+
+-- 글편집하기
+DELIMITER //
+create procedure edit_post(in titleInput varchar(255), in contentsInput varchar(255), in emailInput varchar(255), in idInput bigint)
+begin
+    declare exit handler for SQLEXCEPTION
+    begin
+        rollback;
+    end;
+    start transaction;
+    update post set title=titleInput, contents=contentsInput where id=idInput;
+    insert author_post(author_id, post_id) values((select id from author where email=emailInput), idInput);
+    commit;
+end //
+DELIMITER ;
+
+-- join하여 데이터 조회
+
