a4112138ab8f4aba09a0b240b286cf97b33b5d40
imujinu <wlsdnrhdwkd@gmail.com>
2025-05-29 16:18:40 +0900

    board.sql

--- 10.board.renewal.sql
+++ 10.board.renewal.sql
@@ -1,3 +1,35 @@
+-- 사용자 테이블 생성
+create table author
+(id bigint auto_increment, email varchar(50) not null,
+name varchar(100), password varchar(255) not null, primary key(id));
+
+
+-- 주소 테이블 생성
+create table address
+(id bigint auto_increment, country varchar(255) not null,
+ city varchar(255) not null, street varchar(255) not null,
+ author_id bigint not null, primary key(id), foreign key(author_id) references author(id));
+
+-- post 테이블 생성
+create table post
+(id bigint auto_increment, title varchar(255) not null, 
+contents varchar(1000), primary key(id));
+
+
+-- 연결 테이블 생성
+create table author_post
+(id bigint auto_increment, author_id bigint not null, post_id bigint not null, 
+primary key(id),
+foreign key(author_id) references author(id), 
+foreign key(post_id) references post(id));
+
+-- 복합키를 이용한 연결 테이블 생성
+create table author_post2
+(auto_increment, author_id bigint not null, post_id bigint not null, 
+primary key(author_id, post_id),
+foreign key(author_id) references author(id), 
+foreign key(post_id) references post(id));
+
 -- 회원가입 및 주소생성
 DELIMITER //
 create procedure insert_author(in emailInput varchar(255), in nameInput varchar(255), in passwordInput varchar(255),in countryInput varchar(255), in cityInput varchar(255), in streetInput varchar(255))
@@ -44,5 +76,7 @@ begin
 end //
 DELIMITER ;
 
--- join하여 데이터 조회
+-- JOIN하여 데이터 조회
+select p.title as '제목', p.contents as '내용', a.name as '이름' from post p inner join author_post ap on p.id=ap.post_id
+inner join author a on a.id=ap.author_id;
 
